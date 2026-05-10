5cb73845e59ffaaf35e6ed598263e791fbcbd90f
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-10 05:50:34 +0300

    update ESLint configuration to extend base config

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { config as baseConfig } from '@repo/eslint-config/base';
 
-export default tseslint.config(
+export default defineConfig(
+  ...baseConfig,
+
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -29,7 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
