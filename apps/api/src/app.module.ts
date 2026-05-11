f8e122644e706dc2c9daf9aab77450d44241754b
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-11 19:37:22 +0300

    remove @nestjs/config dependency and its configuration from AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      cache: true,
-      isGlobal: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
