a58b097088ab5db114cb5c3323a68bd455a76591
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-11 20:07:18 +0300

    integrate EnvModule for environment configuration and update main.ts to use env variables

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvModule } from './config/env.module';
 
 @Module({
-  imports: [],
+  imports: [EnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 
-import './common/env'; // to validate env schema on build
+import 'dotenv/config';
 import { AppModule } from './app.module';
+import { env } from './common/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(env.PORT ?? 3001);
 }
 
 void bootstrap();
