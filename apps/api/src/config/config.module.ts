94f50521f62a9ac7bc033d28cf0c33c1cf064d55
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-11 21:50:55 +0300

    setup and initialize DrizzleORM

--- apps/api/src/config/config.module.ts
+++ apps/api/src/config/config.module.ts
@@ -1,9 +1,24 @@
 import { Global, Module } from '@nestjs/common';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
+import { env } from '../common/utils/env';
+import { KEYS } from '../common/utils/key';
 import { ConfigService } from './config.service';
 
 @Module({
-  providers: [ConfigService],
+  providers: [
+    ConfigService,
+    {
+      provide: KEYS.DATABASE_CONNECTION,
+      useFactory: () => {
+        const pool = new Pool({
+          connectionString: env.DATABASE_URL,
+        });
+        return drizzle({ client: pool });
+      },
+    },
+  ],
 })
 @Global()
 export class ConfigModule {}
