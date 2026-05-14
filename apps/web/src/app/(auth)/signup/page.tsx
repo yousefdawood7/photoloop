623877319a3410dd92352bcfced5910a7bfff965
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-14 17:37:16 +0300

    implement signup page layout with AuthBackground and Separator components

--- apps/web/src/app/(auth)/signup/page.tsx
+++ apps/web/src/app/(auth)/signup/page.tsx
@@ -1,3 +1,13 @@
+import AuthBackground from "@/features/auth/components/auth-background";
+import { Button } from "@repo/ui/components/button";
+import { Separator } from "@repo/ui/components/separator";
+
 export default function Page() {
-  return <div>SignUP</div>;
+  return (
+    <section className="flex justify-between border border-r-2 min-h-svh">
+      <AuthBackground />
+      <Separator orientation="vertical" />
+      <div className="basis-1/2">Hi</div>
+    </section>
+  );
 }
---
+++ apps/web/src/features/auth/components/auth-background.tsx
@@ -0,0 +1,3 @@
+export default function AuthBackground() {
+  return <div className="basis-1/2">Auth Background</div>;
+}
