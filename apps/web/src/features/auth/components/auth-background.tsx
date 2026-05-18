54ce9399dd006116a3eadd1376483c3583af4a8e
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-18 21:40:25 +0300

    add <Silk /> component for auth-background

--- apps/web/src/features/auth/components/auth-background.tsx
+++ apps/web/src/features/auth/components/auth-background.tsx
@@ -1,3 +1,18 @@
+import AuthMessage from "@/features/auth/components/auth-message";
+import Silk from "@repo/ui/components/Silk";
+
 export default function AuthBackground() {
-  return <div className="basis-1/2">Auth Background</div>;
+  return (
+    <div className="relative w-full max-w-[500px] xl:max-w-[700px]">
+      <AuthMessage />
+
+      <Silk
+        speed={5}
+        scale={0.8}
+        color="#363636"
+        noiseIntensity={0}
+        rotation={6.02}
+      />
+    </div>
+  );
 }
