f83fc730bacf5113e6bff824fa60866234dd0c01
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-20 02:58:42 +0300

    add registration page and form fields for authentication

--- apps/web/src/features/auth/components/auth-fields/form-fields.tsx
+++ apps/web/src/features/auth/components/auth-fields/form-fields.tsx
@@ -1,5 +1,6 @@
 import FormField from "@/features/auth/components/auth-fields/form-field";
-import { FieldGroup } from "@repo/ui/components/ui/field";
+import { Button } from "@repo/ui/components/button";
+import { Field, FieldGroup } from "@repo/ui/components/ui/field";
 
 export default function FormFields() {
   return (
@@ -11,6 +12,16 @@ export default function FormFields() {
           type="password"
           placeholder="Enter your password"
         />
+
+        <Field>
+          <Button>Sigin in</Button>
+        </Field>
+
+        <footer className="flex justify-between items-center">
+          {/* TODO */}
+          <p>Don't have an account? Sign up</p>
+          <p>Forgot password?</p>
+        </footer>
       </FieldGroup>
     </form>
   );
