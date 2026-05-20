ba1a8fe32c023cc22428359633438121a60834b7
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-20 04:47:49 +0300

    add view transition effects to authentication pages and forms

--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -1,7 +1,15 @@
-import AuthBackground from "@/features/auth/components/auth-background";
 import AuthForms from "@/features/auth/components/auth-forms";
-import { Separator } from "@repo/ui/components/separator";
+import { ViewTransition } from "react";
 
 export default function Page() {
-  return <AuthForms isRegister />;
+  return (
+    <ViewTransition
+      name="auth-form"
+      default="none"
+      enter="auth-enter"
+      exit="auth-exit"
+    >
+      <AuthForms isRegister />
+    </ViewTransition>
+  );
 }
--- apps/web/src/features/auth/components/auth-forms.tsx
+++ apps/web/src/features/auth/components/auth-forms.tsx
@@ -1,3 +1,4 @@
+import { ViewTransition } from "react";
 import FormFields from "@/features/auth/components/auth-fields/form-fields";
 import AuthHeader from "@/features/auth/components/auth-header";
 import AuthMethods from "@/features/auth/components/auth-methods/auth-methods";
@@ -15,16 +16,20 @@ export default function AuthForms({ isRegister = false }: AuthFormsProps) {
 
       <aside className="my-auto flex flex-col gap-5 relative w-full max-w-[500px]">
         <div className=" space-y-2.5 w-full">
-          {isRegister ? (
-            <h2 className="text-3xl">Create an account</h2>
-          ) : (
-            <h2 className="text-3xl">Welcome back</h2>
-          )}
-          <p className="text-muted-foreground">
-            {isRegister
-              ? "Sign up to create your account"
-              : "Sign in to your account to continue your journey with Photoloop"}
-          </p>
+          <ViewTransition name="auth-title">
+            {isRegister ? (
+              <h2 className="text-3xl">Create an account</h2>
+            ) : (
+              <h2 className="text-3xl">Welcome back</h2>
+            )}
+          </ViewTransition>
+          <ViewTransition name="auth-subtitle">
+            <p className="text-muted-foreground">
+              {isRegister
+                ? "Sign up to create your account"
+                : "Sign in to your account to continue your journey with Photoloop"}
+            </p>
+          </ViewTransition>
         </div>
         <AuthMethods />
         <AuthSeparator />
