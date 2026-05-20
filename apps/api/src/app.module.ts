018c1b05d833938e1de56d7619ad30b735862c1f
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-20 19:36:28 +0300

    add email verification functionality using Resend service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { render } from '@react-email/render';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { ResendModule } from 'nestjs-resend';
+import { ResendModule, ResendService } from 'nestjs-resend';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { schema } from './common/db/schemas';
+import { VerifyEmailTemplate } from './common/emails/verify-email';
 import { KEYS } from './common/utils/key';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
@@ -16,7 +18,7 @@ import { ConfigService } from './config/config.service';
   imports: [
     ConfigModule,
     AuthModule.forRootAsync({
-      useFactory: (drizzle: NodePgDatabase) => ({
+      useFactory: (drizzle: NodePgDatabase, resendService: ResendService) => ({
         auth: betterAuth({
           database: drizzleAdapter(drizzle, {
             provider: 'pg',
@@ -29,9 +31,34 @@ import { ConfigService } from './config/config.service';
             minPasswordLength: 8,
             maxPasswordLength: 128,
           },
+          emailVerification: {
+            sendVerificationEmail: async ({ user }) => {
+              void resendService.send({
+                from: 'Photoloop <hello@yousefdawood.me>',
+                to: user.email,
+                subject: 'Verify your email for Photoloop',
+
+                html: await render(
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                  VerifyEmailTemplate({ name: 'Yousef Dawood', otp: '123456' }),
+                ),
+
+                // attachments: [
+                //   {
+                //     path: 'https://i.ibb.co/67wqpNFY/logomark-1.png',
+                //     filename: 'logo.png',
+                //     inlineContentId: 'logo-image',
+                //   },
+                // ],
+              });
+            },
+            sendOnSignUp: true,
+            sendOnSignIn: true,
+            autoSignInAfterVerification: true,
+          },
         }),
       }),
-      inject: [KEYS.DATABASE_CONNECTION],
+      inject: [KEYS.DATABASE_CONNECTION, ResendService],
     }),
 
     ResendModule.forRootAsync({
