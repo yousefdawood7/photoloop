fbed0980913170a16749ab9fefc22e79823ee733
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-21 20:57:06 +0300

    add Facebook authentication option with icon and wordmark

--- apps/web/src/features/auth/components/auth-methods/auth-methods.tsx
+++ apps/web/src/features/auth/components/auth-methods/auth-methods.tsx
@@ -1,6 +1,7 @@
 import AuthButton from "@/features/auth/components/auth-methods/auth-button";
 import { GithubDark } from "@repo/ui/components/svgs/githubDark";
 import { Google } from "@repo/ui/components/svgs/google";
+import { FacebookIcon as Facebook } from "@repo/ui/components/svgs/facebookIcon";
 import { LucideMail } from "@repo/ui/components/icons";
 
 export default function AuthMethods() {
@@ -8,6 +9,11 @@ export default function AuthMethods() {
     <article className="grid grid-cols-2 gap-5">
       <AuthButton title="Sign in with Github" authLogo={<GithubDark />} />
       <AuthButton title="Sign in with Google" authLogo={<Google />} />
+      <AuthButton
+        title="Sign in with Facebook"
+        authLogo={<Facebook />}
+        className="col-span-2"
+      />
       <AuthButton
         title="Sign in with Magic Link"
         authLogo={<LucideMail />}
