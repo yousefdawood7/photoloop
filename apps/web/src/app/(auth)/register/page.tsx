bb4dcf36bb3be3f75675973482cd3cce59bf1bbb
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-21 23:29:35 +0300

    colocate <ViewTransition /> to exclude the header from the page

--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -2,14 +2,5 @@ import AuthForms from "@/features/auth/components/auth-forms";
 import { ViewTransition } from "react";
 
 export default function Page() {
-  return (
-    <ViewTransition
-      name="auth-form"
-      default="none"
-      enter="auth-enter"
-      exit="auth-exit"
-    >
-      <AuthForms isRegister />
-    </ViewTransition>
-  );
+  return <AuthForms isRegister />;
 }
