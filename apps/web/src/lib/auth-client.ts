7d3ba444dd44e0d2047238190ed319f8c47c2970
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-25 02:13:28 +0300

    setup google's one tap on the client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,5 +1,22 @@
+import { env } from "@/lib/env";
+import { oneTapClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: env.BETTER_AUTH_URL,
+
+  plugins: [
+    oneTapClient({
+      clientId: env.GOOGLE_CLIENT_ID,
+
+      autoSelect: false,
+      cancelOnTapOutside: true,
+      context: "signin",
+
+      promptOptions: {
+        baseDelay: 1000,
+        maxAttempts: 5,
+      },
+    }),
+  ],
 });
