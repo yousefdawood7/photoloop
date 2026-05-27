6706369689c882a8a3c60fb755bd1dc2ea5f1422
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-27 10:48:28 +0300

    add lint:fix script to turporepo

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,9 +4,9 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
-import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
 import { config as baseConfig } from "./base.js";
+import globals from "globals";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
