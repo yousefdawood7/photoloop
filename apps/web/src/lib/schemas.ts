cb221f4785fe2ed1bff9a87ea485766d84156301
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-05 08:45:00 +0300

    fix authSchema to support login and register schemas

--- apps/web/src/lib/schemas.ts
+++ apps/web/src/lib/schemas.ts
@@ -1,30 +1,29 @@
 import { z } from "zod";
 
-const authSchema = z
-  .object({
-    name: z
-      .string({ error: "Name is required" })
-      .min(3, { error: "Name should be at least 3 characters" }),
+const authSchema = z.object({
+  name: z
+    .string({ error: "Name is required" })
+    .min(1, { error: "Name is required" }) // for handling empty string case
+    .min(3, { error: "Name should be at least 3 characters" }),
 
-    email: z
-      .string({ error: "Email is required" })
-      .min(1, { error: "Email is required" }) // for handling empty string case
-      .pipe(z.email({ error: "Invalid email format" })),
+  email: z
+    .string({ error: "Email is required" })
+    .min(1, { error: "Email is required" }) // for handling empty string case
+    .pipe(z.email({ error: "Invalid email format" })),
 
-    password: z.coerce
-      .string<string>({ error: "Password is required" })
-      .min(8, { error: "Password must be at least 8 characters long" }),
+  password: z.coerce
+    .string<string>({ error: "Password is required" })
+    .min(8, { error: "Password must be at least 8 characters long" }),
 
-    confirmPassword: z.coerce
-      .string<string>({ error: "Please confirm your password" })
-      .min(1, { error: "Please confirm your password" }),
-  })
-  .refine((val) => val.password === val.confirmPassword, {
-    error: "Password don't match",
-    path: ["confirmPassword"],
-  });
+  confirmPassword: z.coerce
+    .string<string>({ error: "Please confirm your password" })
+    .min(1, { error: "Please confirm your password" }),
+});
 
-export const loginSchema = authSchema;
+export const loginSchema = authSchema.partial({
+  name: true,
+  confirmPassword: true,
+});
 
 export const magicLinkSchema = z.object({
   name: z
