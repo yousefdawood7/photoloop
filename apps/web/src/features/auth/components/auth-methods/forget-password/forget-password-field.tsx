4ef29f958d5ffdd8fa903f7d76f13631cc3fa303
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-08 02:47:11 +0300

    refactor ForgetPassword component to wrap content in ForgetPasswordEmailForm for better structure

--- apps/web/src/features/auth/components/auth-methods/forget-password/forget-password-field.tsx
+++ apps/web/src/features/auth/components/auth-methods/forget-password/forget-password-field.tsx
@@ -7,6 +7,7 @@ import {
   DialogTrigger,
 } from "@repo/ui/components/dialog";
 import FormField from "@/features/auth/components/auth-fields/form-field";
+import ForgetPasswordEmailForm from "@/features/auth/components/auth-methods/forget-password/forget-password-email-form";
 
 export default function ForgetPassword() {
   return (
@@ -15,22 +16,24 @@ export default function ForgetPassword() {
         <p className="cursor-pointer">Forget password</p>
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Reset your password</DialogTitle>
-          <DialogDescription>
-            Enter the email address associated with your account. We&apos;ll
-            send you a secure link to verify your request and create a new
-            password.
-          </DialogDescription>
-        </DialogHeader>
+        <ForgetPasswordEmailForm>
+          <DialogHeader>
+            <DialogTitle>Reset your password</DialogTitle>
+            <DialogDescription>
+              Enter the email address associated with your account. We&apos;ll
+              send you a secure link to verify your request and create a new
+              password.
+            </DialogDescription>
+          </DialogHeader>
 
-        <FormField
-          name="email"
-          label="Email"
-          type="email"
-          placeholder="Enter your email"
-          isSingleField
-        />
+          <FormField
+            name="email"
+            label="Email"
+            type="email"
+            placeholder="Enter your email"
+            isSingleField
+          />
+        </ForgetPasswordEmailForm>
       </DialogContent>
     </Dialog>
   );
