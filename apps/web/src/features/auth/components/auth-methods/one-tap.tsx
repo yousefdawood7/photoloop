f016e0f8f1b6670a11b189d0a32c56d58390c4e0
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-08 06:40:14 +0300

    implement success and error toasts for Google One Tap sign-in

--- apps/web/src/features/auth/components/auth-methods/one-tap.tsx
+++ apps/web/src/features/auth/components/auth-methods/one-tap.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
+import { toast } from "@repo/ui/lib/sonner";
 import { authClient } from "@/lib/auth-client";
 
 export default function OneTap() {
   useEffect(() => {
-    authClient.oneTap();
+    authClient.oneTap({
+      fetchOptions: {
+        onSuccess() {
+          toast.success("You signed in successfully");
+        },
+
+        onError() {
+          toast.error("Failed to sign with goole one tap");
+        },
+      },
+    });
   }, []);
 
   return null;
